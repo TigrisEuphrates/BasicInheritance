2e50d017104f793fb599bf0a7cb62b796439d227
Tigris <tabula.tenebris@gmail.com>
2021-09-21 06:41:41 +0600

    Add project files.

---
+++ BasicInheritance/MiniVan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    sealed class MiniVan:Car
+    {
+        public MiniVan()
+        {
+            
+        }
+    }
+    //Cannot derive from sealed type
+    //class MyString : String { }
+}
---
+++ BasicInheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(80);
+            myCar.Speed = 50;
+            Console.WriteLine($"My car is going {myCar.Speed} MPH");
+            Console.WriteLine();
+
+            MiniVan myMV = new MiniVan();
+            myMV.Speed = 100;
+            Console.WriteLine($"My van is going {myMV.Speed} MPH");
+            Console.WriteLine();
+        }
+    }
+}
